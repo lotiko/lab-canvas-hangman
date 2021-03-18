7e43b4ac020937268c0c0dae29df1c76e29ef12f
lotiko <l.despres@netc.fr>
2021-03-18 20:37:28 +0100

    start iteration one

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,11 +1,16 @@
 class Hangman {
   constructor(words) {
     this.words = words;
+    this.secretWord = this.pickWord();
+    this.letters = [];
+    this.guessedLetters = "";
+    this.errorsLeft = 8;
     // ... your code goes here
   }
 
   pickWord() {
     // ... your code goes here
+    return words[Math.floor(Math.random() * this.words.length)];
   }
 
   checkIfLetter(keyCode) {
@@ -35,11 +40,11 @@ class Hangman {
 
 let hangman;
 
-const startGameButton = document.getElementById('start-game-button');
+const startGameButton = document.getElementById("start-game-button");
 
 if (startGameButton) {
-  startGameButton.addEventListener('click', event => {
-    hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
+  startGameButton.addEventListener("click", (event) => {
+    hangman = new Hangman(["node", "javascript", "react", "miami", "paris", "amsterdam", "lisboa"]);
 
     // HINT (uncomment when start working on the canvas portion of the lab)
     // hangman.secretWord = hangman.pickWord();
@@ -49,7 +54,7 @@ if (startGameButton) {
   });
 }
 
-document.addEventListener('keydown', event => {
+document.addEventListener("keydown", (event) => {
   // React to user pressing a key
   // ... your code goes here
 });
